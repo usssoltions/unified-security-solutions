2d77f9f4c6ee4b366ca0df1beefb983dd5d37031
sales <sales@unifiedbusiness.co.za>
2026-01-14 17:23:10 +0000

    File changes

--- src/pages/PTT.jsx
+++ src/pages/PTT.jsx
@@ -339,6 +339,7 @@ export default function PTT() {
               </div>
             </DialogContent>
           </Dialog>
+          </div>
         </div>
 
         <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
